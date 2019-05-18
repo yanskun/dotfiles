6c8aa1c1211f1aa19b00d601bd13963792a8f8c8
yasudanaoya <naoya.yasuda@seattleconsulting.co.jp>
2019-05-18 19:30:44 +0900

    change: shell name

--- dotfile.sh
+++ dotfile.sh
@@ -1,9 +1,8 @@
 echo 'Paste symbolic link to home directory'
 ln -s dotfiles/zsh/.zshenv ${HOME}/.zshenv
-echo 'Please restart the terminal'
 
-echo 'Run install Homebrew'
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo 'Run app install on homeberw'
 brew bundle
+
 echo 'Finish!!!!!'
+echo 'Please restart the terminal'
