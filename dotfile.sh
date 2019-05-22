fc7c77bd017955e0634aff9c0e8b93ff99f5c3ab
yasudanaoya <naoya.yasuda@seattleconsulting.co.jp>
2019-05-22 10:51:36 +0900

    add: synbolic link gitconfig

--- dotfile.sh
+++ dotfile.sh
@@ -6,6 +6,7 @@ brew bundle
 
 echo 'Paste symbolic link to home directory'
 ln -s ${DOT_DIR}/zsh/.zshenv ${HOME}/.zshenv
+ln -s ${HOME}/dotfiles/.gitconfig ${HOME}/.gitconfig
 
 if [ -e ${HOME}/Library/Application\ Support/Code/User ]; then
   rm -f ${HOME}/Library/Application\ Support/Code/User/settings.json
