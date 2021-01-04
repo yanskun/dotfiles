08ad607a46f72bd651be4175984f0c35568062c8
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-01-04 17:34:55 +0900

    add peco setting

--- dotfile.sh
+++ dotfile.sh
@@ -10,7 +10,12 @@ ln -s ${DOT_DIR}/zsh/.zshenv ${HOME}/.zshenv
 ln -s ${DOT_DIR}/.gitconfig ${HOME}/.gitconfig
 ln -s ${DOT_DIR}/.gitignore_global ${HOME}/.gitignore_global
 
-if [ -e ${HOME}/Library/Application\ Support/Code/User ]; then
+if [[ ! -e ${HOME}/.config/peco ]]; then
+  mkdir ${HOME}/.config/peco
+fi
+ln -s ${DOT_DIR}/peco/config.json ${HOME}/.config/peco/config.json
+
+if [ ! -e ${HOME}/Library/Application\ Support/Code/User ]; then
   echo 'vscode'
   rm -f ${HOME}/Library/Application\ Support/Code/User/settings.json
 
