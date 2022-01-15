31455b4be42d97f72e8936464db36f23e490d952
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-01-16 01:55:31 +0900

    fix: provider

--- vim/lua/settings/_provider.lua
+++ vim/lua/settings/_provider.lua
@@ -1,2 +1,2 @@
-python_host_prog = vim.env.PYENV_ROOT .. '/versions/neovim-2/bin/python'
-python3_host_prog = vim.env.PYENV_ROOT .. '/versions/neovim-3/bin/python'
+vim.g['python_host_prog'] = vim.env.PYENV_ROOT .. '/versions/neovim-2/bin/python'
+vim.g['python3_host_prog'] = vim.env.PYENV_ROOT .. '/versions/neovim-3/bin/python'
