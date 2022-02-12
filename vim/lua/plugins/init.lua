2795b2f168534e2910f40fb2d8776a427b58f015
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-02-13 02:37:10 +0900

    feat: add null-ls config;

--- vim/lua/plugins/init.lua
+++ vim/lua/plugins/init.lua
@@ -135,17 +135,7 @@ require'packer'.startup {function(use)
   use {
     'jose-elias-alvarez/null-ls.nvim',
     requires = 'nvim-lua/plenary.nvim',
-    config = function()
-      local null_ls = require('null-ls')
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.code_actions.gitsigns,
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.completion.spell,
-        }
-      }
-    end
+    config = conf 'null-ls'
   }
 
   -- autocompletion
---
+++ vim/lua/plugins/null-ls.lua
@@ -0,0 +1,21 @@
+return function()
+  local null_ls = require('null-ls')
+  null_ls.setup {
+    sources = {
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.completion.spell,
+    },
+    on_attach = function(client)
+      if client.resolved_capabilities.document_formatting then
+        vim.cmd([[
+          augroup LspFormatting
+            autocmd! * <buffer>
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+          augroup END
+        ]])
+      end
+    end,
+  }
+end
