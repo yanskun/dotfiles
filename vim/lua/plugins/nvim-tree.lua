ae0a2056f18f1b3725d4a96b0ae9e89e3aa642e1
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-02-13 23:03:51 +0900

    fix: nvim-tree, which-key conf

--- vim/lua/plugins/nvim-tree.lua
+++ vim/lua/plugins/nvim-tree.lua
@@ -4,9 +4,9 @@ return function()
     disable_netrw = false,
     hijack_netrw = false,
     hijack_cursor = true,
+  }
 
-    require('which-key').register {
-      ['<leader>t'] = { '<Cmd>NvimTreeToggle<CR>', 'nvim tree open / close toggle' }
-    }
+  require('which-key').register {
+    ['<leader>t'] = { '<Cmd>NvimTreeToggle<CR>', 'nvim tree open / close toggle' }
   }
 end
