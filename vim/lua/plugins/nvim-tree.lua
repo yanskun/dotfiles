c184e12c99cddf7a7c224fb41628b8b4c7f3923b
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-03-28 01:41:45 +0900

    feat: remove auto_close

--- vim/lua/plugins/nvim-tree.lua
+++ vim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,17 @@
 return function()
   require('nvim-tree').setup {
-    auto_close = true,
     disable_netrw = true,
     hijack_netrw = true,
     hijack_cursor = true,
+    -- auto_close = true,
+    update_to_buf_dir = {
+      enable = true,
+      auto_open = true
+    },
+    update_focused_file = {
+      enable = true,
+      update_cwd = false,
+    },
     actions = {
       open_file = {
         resize_window = true,
