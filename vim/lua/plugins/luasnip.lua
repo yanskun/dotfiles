a9cdb9fd1c97dfcd83afcceb45560fcbbae40925
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-04-27 10:56:36 +0900

    feat: add snippets

--- vim/lua/plugins/luasnip.lua
+++ vim/lua/plugins/luasnip.lua
@@ -1,5 +1,37 @@
 return function()
+  local luasnip_ok, ls = pcall(require, "luasnip")
 
-  -- NOTE: load external snippets last so they are not overruled by ls.snippets
+  if not luasnip_ok then
+    return
+  end
+
+
+  local s = ls.snippet
+  local f = ls.function_node
+  local i = ls.insert_node
+  local fmta = require("luasnip.extras.fmt").fmta
+
+  local comment = function()
+    return string.format(vim.bo.commentstring, " TODO: ")
+  end
+
+  ls.add_snippets("all", {
+    s("td", f(comment, { i(0) })),
+  })
+
+  ls.add_snippets("go", {
+    s(
+      "ife",
+      fmta(
+        [[
+  if err != nil {
+    <>
+  }
+  ]],
+        { i(0) }
+      )
+    ),
+  })
+
   require('luasnip.loaders.from_vscode').load()
 end
