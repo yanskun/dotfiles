31adbc529b2d9873649cafad1e00ccb2ede2210a
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-05-17 23:39:17 +0900

    fix: lspconfig

--- .config/vim/lua/plugins/lspconfig.lua
+++ .config/vim/lua/plugins/lspconfig.lua
@@ -11,10 +11,10 @@ return function()
   -- mapping
   require('which-key').register {
     name = 'lsp',
-    ['<space>e'] = { '<cmd>lua vim.diagnostic.open_float()<CR>' },
-    ['[d'] = { '<cmd>lua vim.diagnostic.goto_prev()<CR>' },
-    [']d'] = { '<cmd>lua vim.diagnostic.goto_next()<CR>' },
-    ['<space>q'] = { '<cmd>lua vim.diagnostic.setloclist()<CR>' },
+    ['<space>e'] = { '<cmd>lua vim.diagnostic.open_float()<CR>', 'Show diagnostics in a floating window' },
+    ['[d'] = { '<cmd>lua vim.diagnostic.goto_prev()<CR>', 'Move to the previous diagnostic in the current buffer' },
+    [']d'] = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'Move to the next diagnostic' },
+    ['<space>q'] = { '<cmd>lua vim.diagnostic.setloclist()<CR>', 'Add buffer diagnostics to the location list' },
   }
 
   local servers = {
