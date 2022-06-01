562b9ccf5f80593476535d4f2cb3b21c711ed665
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-01 10:58:49 +0900

    feat: setup flutter tools

--- .config/vim/lua/plugins/flutter-tools.lua
+++ .config/vim/lua/plugins/flutter-tools.lua
@@ -1,5 +1,11 @@
+local util = require('libraries._set_lsp')
+
 return function ()
   require("flutter-tools").setup {
-    flutter_lookup_cmd = 'asdf where flutter'
+    flutter_lookup_cmd = 'asdf where flutter',
+    lsp = {
+      on_attach = util.on_attach,
+      capabilities = util.capabilities
+    }
   }
 end
