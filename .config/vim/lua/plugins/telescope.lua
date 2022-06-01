f979c9edd04b99fbf887bec91534fb473a0b25e6
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-01 10:59:29 +0900

    fix: add telescope options

--- .config/vim/lua/plugins/telescope.lua
+++ .config/vim/lua/plugins/telescope.lua
@@ -16,15 +16,23 @@ return function()
         "node_modules/*",
       }
     },
+    pickers = {
+      live_grep = {
+        additional_args = function()
+          return {"--hidden"}
+        end
+      }
+    }
   }
 
+  local find_command = "{ 'rg', '--files', '--hidden', '-g', '!node_modules/**', '-g', '!.git/**', }"
   require('which-key').register {
     name = 'telescope',
-    ['<leader>ff'] = { "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<cr>", 'telescope find file' },
-    ['<leader>fg'] = { "<cmd>lua require('telescope.builtin').live_grep({ hidden = true })<cr>", 'telescope live grep' },
-    ['<leader>fb'] = { "<cmd>lua require('telescope.builtin').buffers({ hidden = true })<cr>", 'telescope buffers' },
+    ['<leader>ff'] = { "<cmd>lua require('telescope.builtin').find_files({find_command = " .. find_command .. " })<cr>", 'telescope find file' },
+    ['<leader>fg'] = { "<cmd>lua require('telescope.builtin').live_grep({find_command = " .. find_command .. " })<cr>", 'telescope live grep' },
+    ['<leader>fb'] = { "<cmd>lua require('telescope.builtin').buffers({find_command = " .. find_command .. " })<cr>", 'telescope buffers' },
     ['<leader>fh'] = { "<cmd>lua require('telescope.builtin').help_tags()<cr>", 'telescope help tags' },
-    ['<leader>fo'] = { "<cmd>lua require('telescope.builtin').oldfiles({ hidden = true })<cr>", 'telescope old files' },
+    ['<leader>fo'] = { "<cmd>lua require('telescope.builtin').oldfiles({find_command = " .. find_command .. " })<cr>", 'telescope old files' },
     ['<leader>fc'] = { "<cmd>lua require('telescope.builtin').commands()<cr>", 'telescope commands' },
     ['<leader>fk'] = { "<cmd>lua require('telescope.builtin').keymaps()<cr>", 'telescope key maps' },
   }
