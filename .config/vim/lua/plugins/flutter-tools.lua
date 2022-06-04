917fa287fef0a258ccff4594f2cb426814bae341
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-04 14:02:09 +0900

    fix: flutter-tools lspconfig

--- .config/vim/lua/plugins/flutter-tools.lua
+++ .config/vim/lua/plugins/flutter-tools.lua
@@ -1,11 +1,5 @@
-local util = require('libraries._set_lsp')
-
 return function ()
   require("flutter-tools").setup {
     flutter_lookup_cmd = 'asdf where flutter',
-    lsp = {
-      on_attach = util.on_attach,
-      capabilities = util.capabilities
-    }
   }
 end
