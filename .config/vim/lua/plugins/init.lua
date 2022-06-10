c3c2066f3bab6a53170d7796d0701bd26cecec62
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-10 11:44:38 +0900

    feat: use neo-tree

--- .config/vim/lua/plugins/init.lua
+++ .config/vim/lua/plugins/init.lua
@@ -76,10 +76,20 @@ require'packer'.startup {function(use)
   }
 
   -- explorer
+  -- use {
+  --   'kyazdani42/nvim-tree.lua',
+  --   requires = { 'kyazdani42/nvim-web-devicons' },
+  --   config = conf 'nvim-tree'
+  -- }
   use {
-    'kyazdani42/nvim-tree.lua',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = conf 'nvim-tree'
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = conf 'neo-tree',
   }
 
   -- tabline
