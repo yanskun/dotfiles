0f50f6d3fa2fe284b835e7b5e16cce92430ca79f
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-23 22:15:20 +0900

    fix: neo tree

--- .config/vim/lua/plugins/neo-tree.lua
+++ .config/vim/lua/plugins/neo-tree.lua
@@ -185,5 +185,9 @@ return function()
     }
   }
 
-  vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+  -- vim.cmd Your <leader> key ([[nnoremap \ :Neotree reveal<cr>]])
+  require('which-key').register {
+    ['<leader>t'] = { '<Cmd>Neotree toggle<cr>', 'Neotree toggle open / close' },
+    ['<leader>e'] = { '<Cmd>Neotree reveal<cr>', 'Neotree focus explorer' },
+  }
 end
