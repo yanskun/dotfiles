04521ca86c70181a0384ec0ac43c34eb7296654c
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-07-03 10:55:25 +0900

    fix: server name in lualine

--- .config/vim/lua/plugins/lualine.lua
+++ .config/vim/lua/plugins/lualine.lua
@@ -8,6 +8,35 @@ return function()
     return path
   end
 
+  local function client_name()
+    local msg = "No Active Lsp"
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    local client_names = {}
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        client_names[client.name] = true
+      end
+    end
+    if next(client_names) then
+      local prefix = " LSP: "
+      local names = ""
+      for k, _ in pairs(client_names) do
+        if names == "" then
+          names = k
+        else
+          names = names .. "," .. k
+        end
+      end
+      return prefix .. names
+    end
+    return msg
+  end
+
   require('lualine').setup({
     sections = {
       lualine_a = { 'mode' },
@@ -16,9 +45,9 @@ return function()
         filepath,
       },
       lualine_c = {
-        "require'lsp-status'.status()",
         'diagnostics',
         'diff',
+        client_name,
       },
     },
     options = {
