f2abf0744efa0bd23d300cb6a42cf7b1ae8a2893
yasudanaoya <tamago.708@gmail.com>
2022-07-14 15:06:58 +0900

    fix: focus neotree

--- .config/vim/lua/plugins/nvim-tree.lua
+++ .config/vim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,11 @@
 return function()
-  local ok, nvimtree = pcall(require, 'nvim-tree')
+  local ok, nvimtree = pcall(require, "nvim-tree")
 
   if not ok then
     return
   end
 
-  nvimtree.setup {
+  nvimtree.setup({
     disable_netrw = false,
     hijack_netrw = true,
     hijack_cursor = true,
@@ -16,15 +16,16 @@ return function()
     actions = {
       open_file = {
         resize_window = true,
-      }
+      },
     },
     git = {
       enable = true,
       ignore = false,
-    }
-  }
+    },
+  })
 
-  require('which-key').register {
-    ['<leader>t'] = { '<Cmd>NvimTreeToggle<CR>', 'nvim tree open / close toggle' }
-  }
+  require("which-key").register({
+    ["<leader>t"] = { "<Cmd>Neotree reveal toggle<CR>", "nvim tree open / close toggle" },
+    ["<leader>e"] = { "<Cmd>Neotree reveal<CR>", "nvim tree focus current file" },
+  })
 end
