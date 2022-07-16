3a520d6e7bd6c4fa37987983aa59849f2a63ba70
yasudanaoya <tamago.708@gmail.com>
2022-07-16 20:52:54 +0900

    feat: add rust-analyser

---
+++ .config/vim/lua/lsp/rust_analyzer.lua
@@ -0,0 +1,33 @@
+-- https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
+
+if vim.fn.exepath("rust-analyzer") ~= "" then
+  local lspconfig = require("lspconfig")
+  local util = require("libraries._set_lsp")
+
+  lspconfig.rust_analyzer.setup({
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
+    -- https://rust-analyzer.github.io/manual.html#nvim-lsp
+    settings = {
+      ["rust-analyzer"] = {
+        assist = {
+          importGranularity = "module",
+          importPrefix = "self",
+        },
+        cargo = {
+          loadOutDirsFromCheck = true
+        },
+        procMacro = {
+          enable = true
+        },
+      }
+    },
+    on_attach = util.on_attach,
+    capabilities = util.capabilities,
+    flags = util.flags,
+  })
+else
+  print("curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-aarch64-apple-darwin.gz | gunzip -c - > ~/.local/bin/rust-analyzer")
+  print("chmod +x ~/.local/bin/rust-analyzer")
+end
--- .config/vim/lua/plugins/lspconfig.lua
+++ .config/vim/lua/plugins/lspconfig.lua
@@ -1,30 +1,34 @@
 return function()
-  local utils = require('libraries._set_config')
+  local utils = require("libraries._set_config")
   local conf_lsp = utils.conf_lsp
 
   vim.diagnostic.config({
-    virtual_text = false
+    virtual_text = false,
   })
   vim.o.updatetime = 250
-  vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+  vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
   -- mapping
-  require('which-key').register {
-    name = 'lsp',
-    ['<space>e'] = { '<cmd>lua vim.diagnostic.open_float()<CR>', 'Show diagnostics in a floating window' },
-    ['[d'] = { '<cmd>lua vim.diagnostic.goto_prev()<CR>', 'Move to the previous diagnostic in the current buffer' },
-    [']d'] = { '<cmd>lua vim.diagnostic.goto_next()<CR>', 'Move to the next diagnostic' },
-    ['<space>q'] = { '<cmd>lua vim.diagnostic.setloclist()<CR>', 'Add buffer diagnostics to the location list' },
-  }
+  require("which-key").register({
+    name = "lsp",
+    ["<space>e"] = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Show diagnostics in a floating window" },
+    ["[d"] = {
+      "<cmd>lua vim.diagnostic.goto_prev()<CR>",
+      "Move to the previous diagnostic in the current buffer",
+    },
+    ["]d"] = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Move to the next diagnostic" },
+    ["<space>q"] = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Add buffer diagnostics to the location list" },
+  })
 
   local servers = {
-    'denols',
-    'gopls',
-    'jsonls',
-    'sumneko_lua',
-    'tflint',
-    'tsserver',
-    'yamlls',
+    "denols",
+    "gopls",
+    "jsonls",
+    "rust_analyzer",
+    "sumneko_lua",
+    "tflint",
+    "tsserver",
+    "yamlls",
   }
 
   for _, lsp in ipairs(servers) do
