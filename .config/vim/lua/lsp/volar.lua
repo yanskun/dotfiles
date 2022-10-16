832b0596926cfd95b99d319ff86c0f405c969051
yanskun <43776161+yanskun@users.noreply.github.com>
2022-10-16 17:08:39 +0900

    fix: volar

--- .config/vim/lua/lsp/volar.lua
+++ .config/vim/lua/lsp/volar.lua
@@ -2,7 +2,7 @@ if vim.fn.exepath('vue-language-server') ~= '' then
   local lspconfig = require('lspconfig')
   local util = require('libraries._set_lsp')
 
-  lspconfig["volar"].setup {
+  lspconfig.volar.setup {
     on_attach = util.on_attach,
     capabilities = util.capabilities,
     flags = util.flags,
