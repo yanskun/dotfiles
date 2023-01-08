d70948f83407d01476cd1e905d3bd09cb9b63c69
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-01-08 21:34:19 +0900

    fix: syntax, add local

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,14 +2,14 @@ ctrlDoublePress = require("ctrlDoublePress")
 
 -- control window size
 hs.window.animationDuration = 0
-units = {
+local units = {
   right50 = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
   left50 = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   top50 = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
   maximum = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
 }
-mash = { "ctrl", "shift", "cmd" }
+local mash = { "ctrl", "shift", "cmd" }
 hs.hotkey.bind(mash, "right", function()
   hs.window.focusedWindow():move(units.right50, nil, true)
 end)
