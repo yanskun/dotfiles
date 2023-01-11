a007bccf91d9bc6443a37cd0d93b7006a827fd1b
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-01-11 10:37:21 +0900

    feat: add git status by telescope

--- .config/vim/lua/plugins/telescope.lua
+++ .config/vim/lua/plugins/telescope.lua
@@ -25,6 +25,33 @@ return function()
     }
   }
 
+  --  see: https://github.com/nvim-telescope/telescope.nvim/issues/605#issuecomment-790805956
+  local previewers = require('telescope.previewers')
+  local builtin = require('telescope.builtin')
+
+  local delta = previewers.new_termopen_previewer {
+    get_command = function(entry)
+      -- this is for status
+      -- You can get the AM things in entry.status. So we are displaying file if entry.status == '??' or 'A '
+      -- just do an if and return a different command
+      if entry.status == '??' or 'A ' then
+        return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
+      end
+
+      -- note we can't use pipes
+      -- this command is for git_commits and git_bcommits
+      return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
+
+    end
+  }
+
+  function My_git_status(opts)
+    opts = opts or {}
+    opts.previewer = delta
+
+    builtin.git_status(opts)
+  end
+
   local find_command = "{ 'rg', '--files', '--hidden', '-g', '!node_modules/**', '-g', '!.git/**', }"
   require('which-key').register({
     name = 'telescope',
@@ -40,7 +67,8 @@ return function()
         'telescope old files' },
       c = { "<CMD>lua require('telescope.builtin').commands()<CR>", 'telescope commands' },
       k = { "<CMD>lua require('telescope.builtin').keymaps()<CR>", 'telescope key maps' },
-      n = { "<CMD>lua require('telescope').extensions.notify.notify()<CR>", 'telescope notify' }
+      n = { "<CMD>lua require('telescope').extensions.notify.notify()<CR>", 'telescope notify' },
+      s = { "<CMD>lua My_git_status()<CR>", 'telescope git status' },
     },
   }, {
     prefix = "<leader>"
