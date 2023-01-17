e01d77682a8c0e25e5e5048d257f03ca5d5c3d93
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-01-17 14:29:28 +0900

    feat: add bufferline group logic

--- .config/vim/lua/plugins/bufferline.lua
+++ .config/vim/lua/plugins/bufferline.lua
@@ -3,7 +3,34 @@ return function()
     options = {
       show_buffer_close_icons = false,
       show_close_icon = false,
-    }
+      groups = {
+        options = {
+          toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+        },
+        items = {
+          {
+            name = "Tests", -- Mandatory
+            highlight = { underline = true, sp = "blue" }, -- Optional
+            priority = 2, -- determines where it will appear relative to other groups (Optional)
+            icon = "", -- Optional
+            matcher = function(buf) -- Mandatory
+              return buf.filename:match('%_test') or buf.filename:match('%_spec')
+            end,
+          },
+          {
+            name = "Docs",
+            highlight = { undercurl = true, sp = "green" },
+            auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+            matcher = function(buf)
+              return buf.filename:match('%.md') or buf.filename:match('%.txt')
+            end,
+            separator = { -- Optional
+              style = require('bufferline.groups').separator.tab
+            }
+          }
+        }
+      }
+    },
   })
 
   require('which-key').register {
@@ -11,8 +38,8 @@ return function()
     ['<leader>gb'] = { '<Cmd>BufferLinePick<CR>', 'bufferline: pick buffer' },
     ['<leader><Tab>'] = { '<Cmd>BufferLineCycleNext<CR>', 'bufferline: next' },
     ['<leader><S-Tab>'] = { '<Cmd>BufferLineCyclePrev<CR>', 'bufferline: previous' },
-    ['[b'] = { '<Cmd>BufferLineMoveNext<CR>', 'bufferline: move next' },
-    [']b'] = { '<Cmd>BufferLineMovePrev<CR>', 'bufferline: move prev' },
+    ['[b'] = { '<Cmd>BufferLineMovePrev<CR>', 'bufferline: move prev' },
+    [']b'] = { '<Cmd>BufferLineMoveNext<CR>', 'bufferline: move next' },
     ['<leader>1'] = { '<Cmd>BufferLineGoToBuffer 1<CR>', 'which_key_ignore' },
     ['<leader>2'] = { '<Cmd>BufferLineGoToBuffer 2<CR>', 'which_key_ignore' },
     ['<leader>3'] = { '<Cmd>BufferLineGoToBuffer 3<CR>', 'which_key_ignore' },
