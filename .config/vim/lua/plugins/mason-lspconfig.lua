a66f39a8ebb5161e5326f34ee17318022868406e
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-01-24 16:17:22 +0900

    fix: mason ensure

--- .config/vim/lua/plugins/mason-lspconfig.lua
+++ .config/vim/lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,22 @@ return function()
   local servers = require("lsp.servers")
 
   require("mason-lspconfig").setup {
-    ensure_installed = servers,
+    ensure_installed = {
+      "cssls",
+      "denols",
+      "gopls",
+      "jsonls",
+      "ruby_ls",
+      "rust_analyzer",
+      "sqlls",
+      "sumneko_lua",
+      "svelte",
+      "tflint",
+      "volar",
+      "yamlls",
+      "zls",
+      "tsserver",
+    },
     automatic_installation = true,
   }
 end
