f830b980a1e29b4e36ce1d04da666c602d1bb6c7
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-07-10 15:42:27 +0900

    fix: neo-tree icons

--- .config/vim/lua/plugins/neo-tree.lua
+++ .config/vim/lua/plugins/neo-tree.lua
@@ -10,7 +10,7 @@ return function()
   vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
   vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
   vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+  vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
   neo_tree.setup({
     close_if_last_window = true,
@@ -59,11 +59,11 @@ return function()
           added = "✚",  -- or "✚", but this is redundant info if you use git_status_colors on the name
           modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
           deleted = "✖", -- this can only be used in the git_status source
-          renamed = "", -- this can only be used in the git_status source
+          renamed = "󰁕", -- this can only be used in the git_status source
           -- Status type
           untracked = "",
           ignored = "",
-          unstaged = "",
+          unstaged = "󰄱",
           staged = "",
           conflict = "",
         },
