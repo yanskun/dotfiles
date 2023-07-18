299e3c2cd43649da5f30d497c17c973143bd3462
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-07-18 10:53:13 +0900

    feat: add pylsp

---
+++ .config/vim/lua/lsp/pylsp.lua
@@ -0,0 +1,16 @@
+if vim.fn.exepath('pylsp') ~= '' then
+  local lspconfig = require('lspconfig')
+  local util = require('libraries._set_lsp')
+
+  lspconfig.pylsp.setup {
+    on_attach = util.on_attach,
+    capabilities = util.capabilities,
+    flags = util.flags,
+  }
+else
+  vim.notify(
+    'pip install python-lsp-server',
+    vim.log.levels.WARN,
+    { title = 'servername' }
+  )
+end
