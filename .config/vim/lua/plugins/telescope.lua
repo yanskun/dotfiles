f49b3bafbdc737a2d7cdd42d4355c79983d5e731
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-08-10 14:21:55 +0900

    fix: telescope icon

--- .config/vim/lua/plugins/telescope.lua
+++ .config/vim/lua/plugins/telescope.lua
@@ -7,22 +7,31 @@ return function()
 
   telescope.setup {
     defaults = {
-      mappings = {
+      mappings             = {
         i = {
           ["<C-h>"] = "which_key"
         }
       },
       file_ignore_patterns = {
         "node_modules/*",
-      }
+      },
+      -- path_display         = function(opts, path)
+      --   local tail = require("telescope.utils").path_tail(path)
+      --   return tail
+      --   -- return string.format("%s (%s)", tail, path)
+      -- end,
+      prompt_prefix        = '  ',
+      layout_config        = {
+        -- prompt_position = "top",
+      },
     },
     pickers = {
       live_grep = {
         additional_args = function()
           return { "--hidden" }
         end
-      }
-    }
+      },
+    },
   }
 
   --  see: https://github.com/nvim-telescope/telescope.nvim/issues/605#issuecomment-790805956
@@ -41,7 +50,6 @@ return function()
       -- note we can't use pipes
       -- this command is for git_commits and git_bcommits
       return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
-
     end
   }
 
