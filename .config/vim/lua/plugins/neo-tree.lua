ac0d0b0d3e4e0c533b34919bfc2e32792842db8a
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-01-09 16:02:55 +0900

    chore: update nvim plugin vers

--- .config/vim/lua/plugins/neo-tree.lua
+++ .config/vim/lua/plugins/neo-tree.lua
@@ -132,7 +132,9 @@ return function()
           --".null-ls_*",
         },
       },
-      follow_current_file = false,            -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = false, -- This will find and focus the file in the active buffer every
+      },
       -- time the current file is changed while the tree is open.
       group_empty_dirs = false,               -- when true, empty folders will be grouped together
       hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -156,7 +158,9 @@ return function()
       },
     },
     buffers = {
-      follow_current_file = true, -- This will find and focus the file in the active buffer every
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every
+      },
       -- time the current file is changed while the tree is open.
       group_empty_dirs = true,    -- when true, empty folders will be grouped together
       show_unloaded = true,
