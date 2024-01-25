ec826db1bda7977498c168ab59e68d1f826ee32b
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-01-25 12:32:56 +0900

    feat: add dapui toggle cmd

--- .config/vim/lua/plugins/dapui.lua
+++ .config/vim/lua/plugins/dapui.lua
@@ -1,16 +1,16 @@
 return function()
-  require('dapui').setup()
+	require("dapui").setup()
 
-  require('which-key').register({
-    d = {
-      u = {
-        name = 'dapui',
-        c = { "<Cmd>lua require'dapui'.close()<CR>", "dapui close" },
-        o = { "<Cmd>lua require'dapui'.open()<CR>", "dapui open" },
-      }
-    }
-  }, {
-    prefix = '<leader>',
-  }
-  )
+	require("which-key").register({
+		d = {
+			u = {
+				name = "dapui",
+				c = { "<Cmd>lua require'dapui'.close()<CR>", "dapui close" },
+				o = { "<Cmd>lua require'dapui'.open()<CR>", "dapui open" },
+				t = { "<Cmd>lua require'dapui'.toggle()<CR>", "dapui toggle" },
+			},
+		},
+	}, {
+		prefix = "<leader>",
+	})
 end
