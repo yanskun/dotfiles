38e58fb5de67e7a49423ee89c7ed83a84f34864d
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-11-23 14:50:37 +0900

    refactor: plugin config files

--- .config/vim/lua/libraries/_set_config.lua
+++ .config/vim/lua/libraries/_set_config.lua
@@ -5,15 +5,15 @@ local fmt = string.format
 local default_config = require("lsp.default")
 
 function M.conf(name)
-	return require(fmt("plugins.%s", name))
+  return require(fmt("plugins.config.%s", name))
 end
 
 function M.conf_lsp(name)
-	local ok, _ = pcall(require, fmt("lsp.%s", name))
+  local ok, _ = pcall(require, fmt("lsp.%s", name))
 
-	if not ok then
-		default_config(name)
-	end
+  if not ok then
+    default_config(name)
+  end
 end
 
 return M
