561cdd87126cb44d19599f893862db25199d1b9b
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2025-01-21 18:49:37 +0900

    fix: rm volar

--- .config/nvim/lua/lsp/volar.lua
+++
@@ -1,8 +0,0 @@
-local lspconfig = require('lspconfig')
-local util = require('libraries._set_lsp')
-
-lspconfig.volar.setup {
-  on_attach = util.on_attach,
-  capabilities = util.capabilities,
-  flags = util.flags,
-}
