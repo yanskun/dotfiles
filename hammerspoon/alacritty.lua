638cc5fbd7bdadc8d8fade5fc131eb43e95827e4
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2025-01-30 10:39:44 +0900

    Revert "refactor: hs alacritty hooks"
    This reverts commit f5115c61a541dde09e4ac8fd8e6fc506f4b2e49e.

--- hammerspoon/alacritty.lua
+++ hammerspoon/alacritty.lua
@@ -7,8 +7,6 @@ local events = eventtap.event.types
 
 local module = {}
 
-local appName = 'Alacritty'
-
 function MoveActiveScreen(app)
   local window = app:focusedWindow()
 
@@ -24,6 +22,7 @@ module.timeFrame = 1
 
 -- what to do when the double tap of ctrl occurs
 module.action = function()
+  local appName = 'alacritty'
   local app = hs.application.find(appName)
 
   if app == nil then
@@ -64,68 +63,31 @@ end
 -- the actual workhorse
 
 module.eventWatcher = eventtap
-    .new({ events.flagsChanged, events.keyDown }, function(ev)
-      -- if it's been too long; previous state doesn't matter
-      if (timer.secondsSinceEpoch() - timeFirstControl) > module.timeFrame then
-        timeFirstControl, firstDown, secondDown = 0, false, false
-      end
-
-      if ev:getType() == events.flagsChanged then
-        if noFlags(ev) and firstDown and secondDown then -- ctrl up and we've seen two, so do action
-          if module.action then
-            module.action()
-          end
-          timeFirstControl, firstDown, secondDown = 0, false, false
-        elseif onlyCtrl(ev) and not firstDown then -- ctrl down and it's a first
-          firstDown = true
-          timeFirstControl = timer.secondsSinceEpoch()
-        elseif onlyCtrl(ev) and firstDown then -- ctrl down and it's the second
-          secondDown = true
-        elseif not noFlags(ev) then          -- otherwise reset and start over
-          timeFirstControl, firstDown, secondDown = 0, false, false
-        end
-      else -- it was a key press, so not a lone ctrl char -- we don't care about it
-        timeFirstControl, firstDown, secondDown = 0, false, false
-      end
-      return false
-    end)
-    :start()
-
--------------
-
--- toggle App opacity
-local transparent = true
-local hotkey = nil
-local function enableAppHotkey()
-  if not hotkey then
-    hotkey = hs.hotkey.bind({ 'cmd' }, 'u', function()
-      local app = hs.application.find(appName)
-      if app.isFrontmost(app) then
-        if transparent then
-          transparent = false
-        else
-          transparent = true
-        end
-      end
-    end)
-  end
-end
-local function disabledAppHotkey()
-  if hotkey then
-    hotkey:disable()
-    hotkey:delete()
-    hotkey = nil
-  end
-end
-
-module.toggleOpacityWacher = hs.application.watcher.new(function(targetAppName, eventType, appObject)
-  if eventType == hs.application.watcher.activated then
-    if targetAppName == appName then
-      enableAppHotkey()
-    else
-      disabledAppHotkey()
+  .new({ events.flagsChanged, events.keyDown }, function(ev)
+    -- if it's been too long; previous state doesn't matter
+    if (timer.secondsSinceEpoch() - timeFirstControl) > module.timeFrame then
+      timeFirstControl, firstDown, secondDown = 0, false, false
     end
-  end
-end)
+
+    if ev:getType() == events.flagsChanged then
+      if noFlags(ev) and firstDown and secondDown then -- ctrl up and we've seen two, so do action
+        if module.action then
+          module.action()
+        end
+        timeFirstControl, firstDown, secondDown = 0, false, false
+      elseif onlyCtrl(ev) and not firstDown then -- ctrl down and it's a first
+        firstDown = true
+        timeFirstControl = timer.secondsSinceEpoch()
+      elseif onlyCtrl(ev) and firstDown then -- ctrl down and it's the second
+        secondDown = true
+      elseif not noFlags(ev) then -- otherwise reset and start over
+        timeFirstControl, firstDown, secondDown = 0, false, false
+      end
+    else -- it was a key press, so not a lone ctrl char -- we don't care about it
+      timeFirstControl, firstDown, secondDown = 0, false, false
+    end
+    return false
+  end)
+  :start()
 
 return module
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,48 @@
 -- see: https://github.com/Hammerspoon/hammerspoon/pull/3638
 
 Alacritty = require('alacritty')
-Alacritty.toggleOpacityWacher:start()
+
+-- control window size
+hs.window.animationDuration = 0
+local units = {
+  right50 = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  left50 = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  top50 = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bot50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  maximum = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+}
+local mash = { 'ctrl', 'shift', 'cmd' }
+hs.hotkey.bind(mash, 'right', function()
+  hs.window.focusedWindow():move(units.right50, nil, true)
+end)
+hs.hotkey.bind(mash, 'left', function()
+  hs.window.focusedWindow():move(units.left50, nil, true)
+end)
+hs.hotkey.bind(mash, 'up', function()
+  hs.window.focusedWindow():move(units.top50, nil, true)
+end)
+hs.hotkey.bind(mash, 'down', function()
+  hs.window.focusedWindow():move(units.bot50, nil, true)
+end)
+hs.hotkey.bind(mash, 'm', function()
+  hs.window.focusedWindow():move(units.maximum, nil, true)
+end)
+
+-- toggle Alacritty opacity
+local transparent = true
+hs.hotkey.bind({ 'cmd' }, 'u', function()
+  local appName = 'alacritty'
+  local app = hs.application.find(appName)
+  if app.isFrontmost(app) then
+    if transparent then
+      hs.execute('alacritty msg config window.opacity=1', true)
+      transparent = false
+    else
+      hs.execute('alacritty msg config --reset', true)
+      transparent = true
+    end
+  end
+end)
 
 -- toggle Keycastr
 hs.hotkey.bind({ 'ctrl', 'shift' }, 'k', function()
@@ -17,8 +58,8 @@ end)
 
 local screenWatcher = hs.screen.watcher.new(function()
   if not hs.fnutils.some(hs.screen.allScreens(), function(scr)
-        return scr:name():find('Built%-in .*')
-      end) then
+    return scr:name():find('Built%-in .*')
+  end) then
     hs.execute('yabai --start-service', true)
   else
     hs.execute('yabai --stop-service', true)
