185aa0004668a38ebe0f02290b9dcac80581c312
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2025-04-02 17:05:48 +0900

    fix: deprecated

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -11,7 +11,7 @@ return function()
   local function client_name()
     local msg = 'No Active Lsp'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
